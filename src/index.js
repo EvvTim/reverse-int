933e8be727d1816c4651bc16a6f66bfae9a562ba
Evgeniy Tsimashchuk <evgeniytim94@gmail.com>
2021-08-22 15:01:01 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-    return parseInt(n.toString().split('').reverse().join(''))
+    return parseInt(n.toString()
+        .split('')
+        .reverse()
+        .join(''))
 }
